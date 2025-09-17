67312bcfb0398a9ca9d8bb5b516e6016b1950013
caio <caiohenriqueviana01@gmail.com>
2025-09-17 20:21:05 -0300

    adicionando state

---
+++ Android/estado/app/src/main/java/ads/mov/MainActivity.kt
@@ -0,0 +1,96 @@
+package ads.mov
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ads.mov.ui.theme.MyApplicationTheme
+import android.view.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Composable
+fun Hidratacao(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .padding(16.dp)
+            .windowInsetsPadding(WindowInsets.statusBars)
+    ){
+       Column(
+           modifier = Modifier.fillMaxSize(),
+           verticalArrangement = Arrangement.Center
+       ) {
+           var contador by remember { mutableIntStateOf(0) }
+           Text(
+               modifier = Modifier.padding(vertical = 16.dp),
+               text = "Você bebeu $contador copos de agua",
+               style = MaterialTheme.typography.headlineSmall
+           )
+           Button(
+               modifier = Modifier.fillMaxWidth(),
+               onClick = { contador++ }
+           ) {
+               Text(
+                   text ="Beber Agua"
+               )
+           }
+       }
+    }
+
+}
+
+@Preview
+@Composable
+private fun PP() {
+    Hidratacao()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}

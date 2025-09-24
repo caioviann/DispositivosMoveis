b4cf74c4709cd0fea62dc635cfea815d52f918ac
caio <caiohenriqueviana01@gmail.com>
2025-09-24 20:15:58 -0300

    desafio 2

--- Android/desafio2/app/src/main/java/ads/mov/MainActivity.kt
+++ Android/desafio2/app/src/main/java/ads/mov/MainActivity.kt
@@ -15,14 +15,18 @@ import ads.mov.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
@@ -67,25 +71,38 @@ fun Dados(modifier: Modifier = Modifier) {
         ){
             var face by rememberSaveable { mutableStateOf(1) }
             var face2 by rememberSaveable { mutableStateOf(1) }
+            var butaoClicado by remember { mutableStateOf(false) }
 
             Button(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = {
                     face = (1..6).random()
                     face2 = (1..6).random()
+                    butaoClicado = true
+
                 }
             ) {
                 Text("Jogar", fontSize = 24.sp)
             }
-            Image(
-                painter = painterResource(Imagem(face)),
-                contentDescription = face.toString()
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Image(
+                    modifier = Modifier.weight(1f),
+                    painter = painterResource(Imagem(face)),
+                    contentDescription = face.toString()
+                )
+                Image(
+                    modifier = Modifier.weight(1f),
+                    painter = painterResource(Imagem(face2)),
+                    contentDescription = face.toString()
+                )
+            }
+            Text(
+                text = RegraDoJogo(face, face2, butaoClicado),
+                style = MaterialTheme.typography.headlineMedium
             )
-            Image(
-                painter = painterResource(Imagem(face2)),
-                contentDescription = face.toString()
-            )
-            Text(RegraDoJogo(face, face2))
         }
     }
 }
@@ -100,14 +117,31 @@ fun Imagem (face: Int, modifier: Modifier = Modifier): Int {
     }
     return imagem
 }
-fun RegraDoJogo(dado1: Int, dado2: Int, modifier: Modifier = Modifier): String{
-    var soma = dado1 + dado2
-    if (soma == 7 || soma == 11){
-        return "Você ganhou"
-    }else if (soma == 2 || soma == 3 || soma == 12){
-        return "Você perdeu"
+@Composable
+fun RegraDoJogo(dado1: Int, dado2: Int,bClicado: Boolean, modifier: Modifier = Modifier): String{
+    var soma: Int = 0
+
+    if (bClicado == true){
+        soma = dado1 + dado2
     }
-    return "Ishh, tente novamente"
+    var ponto = 0
+
+    if (ponto >= 2){
+        if (soma == ponto){
+            return "Agora você ganhou! $ponto"
+        }else if (soma == 7){
+            return "Agora você perdeu na ultima chance"
+        }
+    }else {
+        ponto = soma
+        if (soma == 7 || soma == 11){
+            return "Você ganhou"
+        }else if (soma == 2 || soma == 3 || soma == 12){
+            return "Você perdeu"
+        }
+    }
+
+    return "Ishh, tente novamente $ponto $bClicado"
 }
 
 @Preview(showSystemUi = true, showBackground = true)
